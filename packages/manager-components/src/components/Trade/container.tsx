87f7aa71e1d5b078d984f150f2d02f0208586f40
Matthias Gemperli <hello@matthiasgemperli.ch>
2018-06-25 14:51:37 +0200

    Refactor trade container

--- packages/manager-components/src/components/Trade/container.tsx
+++ packages/manager-components/src/components/Trade/container.tsx
@@ -1,7 +1,6 @@
 import { withState } from 'recompose';
-import compose from 'recompose/compose';
 import Trade from './index';
 
 const withActiveTabState = withState('activeTabIndex', 'setTabIndex', 0);
 
-export default compose(withActiveTabState)(Trade);
+export default (withActiveTabState)(Trade);
