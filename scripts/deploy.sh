6d70fd52ede91c680c8ac2b281d1999f4f7c3a9a
Sebastian Siemssen <fubhy@fubhy.com>
2018-06-26 11:12:47 +0200

    Enable auto deployments.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,3 +3,7 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 make package
 make publish
+
+# TODO: Use rolling updates for this.
+kubectl set image deployment/graphql-live graphql-live=melonproject/graphql-server:${TRAVIS_BUILD_ID}
+kubectl set image deployment/graphql-kovan graphql-kovan=melonproject/graphql-server:${TRAVIS_BUILD_ID}
