a29590290060c469b0d047fa1a9cc7544edcbf95
Matthias Gemperli <hello@matthiasgemperli.ch>
2018-07-04 14:47:04 +0200

    Add new form

--- packages/manager-interface/src/legacy/components/pages/Fund.jsx
+++ packages/manager-interface/src/legacy/components/pages/Fund.jsx
@@ -13,8 +13,9 @@ import GetStarted from '../../containers/GetStarted';
 import Participation from '../../containers/Participation';
 import ExecuteRequest from '../../containers/ExecuteRequest';
 import TradeHelper from '../../containers/TradeHelper';
-import Trade from '../../containers/Trade';
 import OpenOrders from '../../containers/OpenOrders';
+import Layout from '@melonproject/manager-components/design/Layout';
+import Trade from '@melonproject/manager-components/components/Trade/container';
 
 const Fund = ({
   isManager,
@@ -22,43 +23,49 @@ const Fund = ({
   canInvest,
   pendingRequest,
   isCompetition,
+  orderForm,
+  onSubmit,
 }) => (
-    <div className="App">
-      <br />
-      <div>
-        <Card.Group>
-          <Factsheet />
-              {!isCompetition && isManager ? <Administration /> : <GetStarted />}
-              {!isCompetition && canInvest && !pendingRequest ? <Participation /> : <div />}
-              {!isCompetition && canInvest && pendingRequest ? <ExecuteRequest /> : <div />}
-              {!isCompetition && !canInvest ? <Card /> : <div />}
-      
-        </Card.Group>
-        <br />
-        <Holdings address={fundAddress} />
-        <br />
-        {isManager ? (
-          <div>
-            {' '}
-            <Card.Group>
-              <Trade />
-              <TradeHelper />
-            </Card.Group>
-          </div>
+  <div className="App">
+    <br />
+    <div>
+      <Card.Group>
+        <Factsheet />
+        {!isCompetition && isManager ? <Administration /> : <GetStarted />}
+        {!isCompetition && canInvest && !pendingRequest ? (
+          <Participation />
         ) : (
-            <div />
-          )}
-        <br />
-        <Orderbook />
-        <br />
-        <OpenOrders address={fundAddress} />
-        <br />
-        <RecentTrades />
-        <br />
-        <TradeHistory address={fundAddress} />
-      </div>
+          <div />
+        )}
+        {!isCompetition && canInvest && pendingRequest ? (
+          <ExecuteRequest />
+        ) : (
+          <div />
+        )}
+        {!isCompetition && !canInvest ? <Card /> : <div />}
+      </Card.Group>
       <br />
+      <Holdings address={fundAddress} />
+      <br />
+      {isManager ? (
+        <div>
+          <h3>Trade</h3>
+          <Trade form={{ ...orderForm, onSubmit: onSubmit }} />
+        </div>
+      ) : (
+        <div />
+      )}
+      <br />
+      <Orderbook />
+      <br />
+      <OpenOrders address={fundAddress} />
+      <br />
+      <RecentTrades />
+      <br />
+      <TradeHistory address={fundAddress} />
     </div>
-  );
+    <br />
+  </div>
+);
 
 export default Fund;
