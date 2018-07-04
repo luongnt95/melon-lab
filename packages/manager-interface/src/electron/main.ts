b7c055459a3045aba70815af402507c1ef4a54c6
Matthias Gemperli <hello@matthiasgemperli.ch>
2018-07-04 10:03:56 +0200

    Enable debug

--- packages/manager-interface/src/electron/main.ts
+++ packages/manager-interface/src/electron/main.ts
@@ -12,9 +12,7 @@ import startServer from './server';
 const isWindows = process.platform === 'win32';
 const isMac = process.platform === 'darwin';
 
-if (isDev) {
-  debug({ enabled: true, showDevTools: true });
-}
+debug({ enabled: true, showDevTools: true });
 
 const appUrl = async () => {
   if (!isDev) {
