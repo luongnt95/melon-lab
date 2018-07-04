b430bd3341e2c70be8ae3eb2be8df668fd8e29a7
Jenna Zenk <zenkjenna@gmail.com>
2018-07-04 15:44:52 +0200

    Protocol version bump

--- packages/melon.js/lib/version/calls/getConfig.js
+++ packages/melon.js/lib/version/calls/getConfig.js
@@ -55,7 +55,7 @@ const getConfig = async (environment, optionalNetwork): Promise<Config> => {
     ? optionalNetwork.toLowerCase()
     : await getNetwork(environment);
   config = {
-    onlyManagerAddress: addressBook[network].OnlyManager,
+    onlyManagerAddress: addressBook[network].OnlyManagerCompetition,
     competitionComplianceAddress: addressBook[network].CompetitionCompliance,
     matchingMarketAddress: addressBook[network].MatchingMarket,
     matchingMarketAdapter: addressBook[network].MatchingMarketAdapter,
@@ -63,7 +63,7 @@ const getConfig = async (environment, optionalNetwork): Promise<Config> => {
     zeroExV1AdapterAddress: addressBook[network].ZeroExV1Adapter,
     canonicalPriceFeedAddress: addressBook[network].CanonicalPriceFeed,
     rankingAddress: addressBook[network].FundRanking,
-    riskManagementAddress: addressBook[network].RMMakeOrders,
+    riskManagementAddress: addressBook[network].NoRiskMgmt,
     versionAddress: addressBook[network].Version,
     governanceAddress: addressBook[network].Governance,
     olympiadAddress: addressBook[network].Competition,
