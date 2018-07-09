4eaa5dee90a3200516fec1380d6a1af4a54a2374
Jenna Zenk <zenkjenna@gmail.com>
2018-07-09 16:05:47 +0200

    Format

--- packages/manager-interface/src/legacy/sagas/ethereum.js
+++ packages/manager-interface/src/legacy/sagas/ethereum.js
@@ -1,12 +1,12 @@
 import { take, put, takeLatest, select, apply, call } from 'redux-saga/effects';
 import { eventChannel } from 'redux-saga';
 import {
-    getConfig,
-    onBlock,
-    getParityProvider,
-    providers,
-    setEnvironment,
-    getEnvironment,
+  getConfig,
+  onBlock,
+  getParityProvider,
+  providers,
+  setEnvironment,
+  getEnvironment,
 } from '@melonproject/melon.js';
 import { utils } from 'ethers';
 
@@ -21,100 +21,100 @@ const BLOCK_POLLING_INTERVAL = 4 * 1000;
 const MAX_INTERVAL_BETWEEN_BLOCKS = 5;
 
 function* init() {
-    const environment = yield call(getParityProvider, process.env.JSON_RPC_ENDPOINT);
+  const environment = yield call(getParityProvider, process.env.JSON_RPC_ENDPOINT);
 
-    // TODO: add tracer
-    setEnvironment(environment);
-    yield put(ethereumActions.setProvider(environment.providerType));
-    const track = yield select(state => state.app.track);
-    const config = yield call(getConfig, environment, track);
+  // TODO: add tracer
+  setEnvironment(environment);
+  yield put(ethereumActions.setProvider(environment.providerType));
+  const track = yield select(state => state.app.track);
+  const config = yield call(getConfig, environment, track);
 
-    global.MELON_PROTOCOL_CONFIG = config;
-    yield put(fundActions.setConfig(config));
-    yield put(
-        appActions.updateAssetPair({
-            base: 'MLN-T',
-            quote: config.quoteAssetSymbol,
-        }),
+  global.MELON_PROTOCOL_CONFIG = config;
+  yield put(fundActions.setConfig(config));
+  yield put(
+    appActions.updateAssetPair({
+      base: 'MLN-T',
+      quote: config.quoteAssetSymbol,
+    }),
+  );
+
+  // Reading the fund address from the URL
+  const fund = yield select(state => state.fund);
+  const networkId = yield apply(environment.api, environment.api.net.version);
+
+  yield put(ethereumActions.hasConnected(networkId));
+
+  if (fund.address !== '' && fund.name === '-') {
+    yield put(fundActions.infoRequested(fund.address));
+  }
+
+  const walletString = localStorage.getItem('wallet:melon.fund');
+  if (process.env.NODE_ENV === 'development' && walletString) {
+    console.warn(
+      'Loading unencrypted wallet from localStorage. This should only happen in development and with playmoney (i.e. kovan)',
     );
+    const wallet = JSON.parse(walletString);
+    environment.account = wallet;
+    setEnvironment(environment);
+    yield put(walletActions.importWalletSucceeded(wallet));
+    yield put(ethereumActions.accountChanged(`${wallet.address}`));
+  }
 
-    // Reading the fund address from the URL
-    const fund = yield select(state => state.fund);
-    const networkId = yield apply(environment.api, environment.api.net.version);
+  const blockChannel = eventChannel(emitter => {
+    let lastBlockNumber;
+    let intervalsSinceLastBlock = 0;
 
-    yield put(ethereumActions.hasConnected(networkId));
+    const pollBlock = async () => {
+      try {
+        const blockNumber = await environment.api.eth.blockNumber();
 
-    if (fund.address !== '' && fund.name === '-') {
-        yield put(fundActions.infoRequested(fund.address));
-    }
-
-    const walletString = localStorage.getItem('wallet:melon.fund');
-    if (process.env.NODE_ENV === 'development' && walletString) {
-        console.warn(
-            'Loading unencrypted wallet from localStorage. This should only happen in development and with playmoney (i.e. kovan)',
-        );
-        const wallet = JSON.parse(walletString);
-        environment.account = wallet;
-        setEnvironment(environment);
-        yield put(walletActions.importWalletSucceeded(wallet));
-        yield put(ethereumActions.accountChanged(`${wallet.address}`));
-    }
-
-    const blockChannel = eventChannel(emitter => {
-        let lastBlockNumber;
-        let intervalsSinceLastBlock = 0;
-
-        const pollBlock = async () => {
-            try {
-                const blockNumber = await environment.api.eth.blockNumber();
-
-                if (!equals(blockNumber, lastBlockNumber)) {
-                    const environment = getEnvironment();
-                    const data = await onBlock(environment);
-                    emitter({ onBlock: { ...data, blockNumber } });
-                    lastBlockNumber = blockNumber;
-                    intervalsSinceLastBlock = 0;
-                } else {
-                    intervalsSinceLastBlock += 1;
-                }
-
-                if (intervalsSinceLastBlock > MAX_INTERVAL_BETWEEN_BLOCKS) {
-                    emitter({ blockOverdue: true });
-                }
-            } catch (e) {
-                emitter({ blockError: true });
-                console.error(e);
-            }
-        };
-
-        pollBlock();
-        const blockInterval = setInterval(pollBlock, BLOCK_POLLING_INTERVAL);
-
-        return () => {
-            clearInterval(blockInterval);
-        };
-    });
-
-    while (true) {
-        const data = yield take(blockChannel);
-        if (data.onBlock) {
-            yield put(
-                ethereumActions.newBlock({
-                    ...data.onBlock,
-                    mlnBalance: data.onBlock.melonBalance,
-                    ethBalance: data.onBlock.etherBalance,
-                }),
-            );
-        } else if (data.blockOverdue) {
-            yield put(ethereumActions.blockOverdue());
+        if (!equals(blockNumber, lastBlockNumber)) {
+          const environment = getEnvironment();
+          const data = await onBlock(environment);
+          emitter({ onBlock: { ...data, blockNumber } });
+          lastBlockNumber = blockNumber;
+          intervalsSinceLastBlock = 0;
         } else {
-            yield put(ethereumActions.blockError());
+          intervalsSinceLastBlock += 1;
         }
+
+        if (intervalsSinceLastBlock > MAX_INTERVAL_BETWEEN_BLOCKS) {
+          emitter({ blockOverdue: true });
+        }
+      } catch (e) {
+        emitter({ blockError: true });
+        console.error(e);
+      }
+    };
+
+    pollBlock();
+    const blockInterval = setInterval(pollBlock, BLOCK_POLLING_INTERVAL);
+
+    return () => {
+      clearInterval(blockInterval);
+    };
+  });
+
+  while (true) {
+    const data = yield take(blockChannel);
+    if (data.onBlock) {
+      yield put(
+        ethereumActions.newBlock({
+          ...data.onBlock,
+          mlnBalance: data.onBlock.melonBalance,
+          ethBalance: data.onBlock.etherBalance,
+        }),
+      );
+    } else if (data.blockOverdue) {
+      yield put(ethereumActions.blockOverdue());
+    } else {
+      yield put(ethereumActions.blockError());
     }
+  }
 }
 
 function* ethereum() {
-    yield takeLatest(browserTypes.LOADED, init);
+  yield takeLatest(browserTypes.LOADED, init);
 }
 
 export default ethereum;
--- packages/melon.js/lib/version/calls/getConfig.js
+++ packages/melon.js/lib/version/calls/getConfig.js
@@ -14,33 +14,33 @@ import type { TokenSymbol } from '../../assets/schemas/TokenSymbol';
  * Asset config
  */
 export type AssetConfig = {
-    address: Address,
-    decimal: number,
-    name: string,
-    symbol: TokenSymbol,
-    url: string,
+  address: Address,
+  decimal: number,
+  name: string,
+  symbol: TokenSymbol,
+  url: string,
 };
 
 /**
  * Config retrieved from the version
  */
 export type Config = {
-    assets: Array<AssetConfig>,
-    onlyManagerCompetitionAddress: Address,
-    noComplianceCompetitionAddress: Address,
-    competitionComplianceAddress: Address,
-    matchingMarketAddress: Address,
-    matchingMarketAdapter: Address,
-    zeroExV1Address: Address,
-    zeroExV1AdapterAddress: Address,
-    nativeAssetSymbol: TokenSymbol,
-    canonicalPriceFeedAddress: Address,
-    stakingPriceFeedAddress: Address,
-    quoteAssetSymbol: TokenSymbol,
-    rankingAddress: Address,
-    riskManagementAddress: Address,
-    versionAddress: Address,
-    governanceAddress: Address,
+  assets: Array<AssetConfig>,
+  onlyManagerCompetitionAddress: Address,
+  noComplianceCompetitionAddress: Address,
+  competitionComplianceAddress: Address,
+  matchingMarketAddress: Address,
+  matchingMarketAdapter: Address,
+  zeroExV1Address: Address,
+  zeroExV1AdapterAddress: Address,
+  nativeAssetSymbol: TokenSymbol,
+  canonicalPriceFeedAddress: Address,
+  stakingPriceFeedAddress: Address,
+  quoteAssetSymbol: TokenSymbol,
+  rankingAddress: Address,
+  riskManagementAddress: Address,
+  versionAddress: Address,
+  governanceAddress: Address,
 };
 
 let config: Config;
@@ -49,38 +49,38 @@ let config: Config;
  * Get config from deployed version contract
  */
 const getConfig = async (environment, track = "kovan-demo"): Promise<Config> => {
-    if (config) return config;
+  if (config) return config;
 
-    const network = await getNetwork(environment);
+  const network = await getNetwork(environment);
 
-    let mode;
-    if (track === "kovan-demo") mode = "kovan"
-    else if (track === "kovan-competition") mode = "kovanCompetition"
-    else if (track === "live") mode = "live"
+  let mode;
+  if (track === "kovan-demo") mode = "kovan"
+  else if (track === "kovan-competition") mode = "kovanCompetition"
+  else if (track === "live") mode = "live"
 
-    config = {
-        onlyManagerCompetitionAddress: addressBook[mode].OnlyManagerCompetition,
-        competitionComplianceAddress: addressBook[mode].CompetitionCompliance,
-        matchingMarketAddress: addressBook[mode].MatchingMarket,
-        matchingMarketAdapter: addressBook[mode].MatchingMarketAdapter,
-        zeroExV1Address: addressBook[mode].ZeroExExchange,
-        zeroExV1AdapterAddress: addressBook[mode].ZeroExV1Adapter,
-        canonicalPriceFeedAddress: addressBook[mode].CanonicalPriceFeed,
-        rankingAddress: addressBook[mode].FundRanking,
-        riskManagementAddress: addressBook[mode].NoRiskMgmt,
-        versionAddress: addressBook[mode].Version,
-        governanceAddress: addressBook[mode].Governance,
-        olympiadAddress: addressBook[mode].Competition
-    }
+  config = {
+    onlyManagerCompetitionAddress: addressBook[mode].OnlyManagerCompetition,
+    competitionComplianceAddress: addressBook[mode].CompetitionCompliance,
+    matchingMarketAddress: addressBook[mode].MatchingMarket,
+    matchingMarketAdapter: addressBook[mode].MatchingMarketAdapter,
+    zeroExV1Address: addressBook[mode].ZeroExExchange,
+    zeroExV1AdapterAddress: addressBook[mode].ZeroExV1Adapter,
+    canonicalPriceFeedAddress: addressBook[mode].CanonicalPriceFeed,
+    rankingAddress: addressBook[mode].FundRanking,
+    riskManagementAddress: addressBook[mode].NoRiskMgmt,
+    versionAddress: addressBook[mode].Version,
+    governanceAddress: addressBook[mode].Governance,
+    olympiadAddress: addressBook[mode].Competition
+  }
 
-    // HACK: Define config first so that inside these next async functions,
-    // getConfig() already returns the addresses to avoid an infinite loop
-    config.assets = await getWhiteListedAssets(environment, network);
-    config.nativeAssetSymbol = await getNativeAssetSymbol(environment);
-    config.quoteAssetSymbol = await getQuoteAssetSymbol(environment);
-    config.melonAssetSymbol = network === 'kovan' ? 'MLN-T' : 'MLN';
+  // HACK: Define config first so that inside these next async functions,
+  // getConfig() already returns the addresses to avoid an infinite loop
+  config.assets = await getWhiteListedAssets(environment, network);
+  config.nativeAssetSymbol = await getNativeAssetSymbol(environment);
+  config.quoteAssetSymbol = await getQuoteAssetSymbol(environment);
+  config.melonAssetSymbol = network === 'kovan' ? 'MLN-T' : 'MLN';
 
-    return config;
+  return config;
 };
 
 export default getConfig;
