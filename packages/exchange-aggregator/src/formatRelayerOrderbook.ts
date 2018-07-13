d4adac925d7bc3eb8d38573e58e11c764fefc6ae
Sebastian Siemssen <fubhy@fubhy.com>
2018-07-13 11:58:38 +0200

    Fixing invalid token addresses from exchanges. (#145)

--- packages/exchange-aggregator/src/formatRelayerOrderbook.ts
+++ packages/exchange-aggregator/src/formatRelayerOrderbook.ts
@@ -1,76 +1,81 @@
 import { getPrices, getSymbol, toReadable } from '@melonproject/melon.js';
 
 const formatRelayerOrderbook = exchange => (config, bids, asks) => {
-  const formattedBids = bids.filter(order => {
-    if (order.makerTokenAddress === '0x0000000000000000000000000000000000000000') {
-      return false;
-    }
+  const formattedBids = bids.map(order => {
+    try {
+      const makerSymbol = getSymbol(config, order.makerTokenAddress);
+      const takerSymbol = getSymbol(config, order.takerTokenAddress);
+      const makerAmount = order.makerTokenAmount;
+      const takerAmount = order.takerTokenAmount;
 
-    if (order.takerTokenAddress === '0x0000000000000000000000000000000000000000') {
-      return false;
-    }
+      const makerHowMuch = toReadable(config, makerAmount, makerSymbol);
+      const takerHowMuch = toReadable(config, takerAmount, takerSymbol);
 
-    return true;
-  }).map(order => ({
-    salt: order.salt,
-    maker: order.maker,
-    taker: order.taker,
-    isActive: true,
-    sell: {
-      symbol: getSymbol(config, order.makerTokenAddress),
-      howMuch: toReadable(
-        config,
-        order.makerTokenAmount,
-        getSymbol(config, order.makerTokenAddress),
-      ),
-    },
-    buy: {
-      symbol: getSymbol(config, order.takerTokenAddress),
-      howMuch: toReadable(
-        config,
-        order.takerTokenAmount,
-        getSymbol(config, order.takerTokenAddress),
-      ),
-    },
-    type: 'buy',
-    makerFee: order.makerFee,
-    takerFee: order.takerFee,
-    signature: order.ecSignature,
-    expiration: order.expirationUnixTimestampSec,
-    feeRecipient: order.feeRecipient,
-    exchangeContractAddress: order.exchangeContractAddress,
-    exchange,
-  }));
-  const formattedAsks = asks.map(order => ({
-    salt: order.salt,
-    maker: order.maker,
-    taker: order.taker,
-    isActive: true,
-    sell: {
-      symbol: getSymbol(config, order.makerTokenAddress),
-      howMuch: toReadable(
-        config,
-        order.makerTokenAmount,
-        getSymbol(config, order.makerTokenAddress),
-      ),
-    },
-    buy: {
-      symbol: getSymbol(config, order.takerTokenAddress),
-      howMuch: toReadable(
-        config,
-        order.takerTokenAmount,
-        getSymbol(config, order.takerTokenAddress),
-      ),
-    },
-    type: 'sell',
-    makerFee: order.makerFee,
-    takerFee: order.takerFee,
-    signature: order.ecSignature,
-    expiration: order.expirationUnixTimestampSec,
-    feeRecipient: order.feeRecipient,
-    exchangeContractAddress: order.exchangeContractAddress,
-    exchange,
-  }));
+      return {
+        salt: order.salt,
+        maker: order.maker,
+        taker: order.taker,
+        isActive: true,
+        sell: {
+          symbol: makerSymbol,
+          howMuch: makerHowMuch,
+        },
+        buy: {
+          symbol: takerSymbol,
+          howMuch: takerHowMuch,
+        },
+        type: 'buy',
+        makerFee: order.makerFee,
+        takerFee: order.takerFee,
+        signature: order.ecSignature,
+        expiration: order.expirationUnixTimestampSec,
+        feeRecipient: order.feeRecipient,
+        exchangeContractAddress: order.exchangeContractAddress,
+        exchange,
+      }
+    }
+    catch (e) {
+      return undefined;
+    }
+  }).filter(order => typeof order !== 'undefined');
+
+  const formattedAsks = asks.map(order => {
+    try {
+      const makerSymbol = getSymbol(config, order.makerTokenAddress);
+      const takerSymbol = getSymbol(config, order.takerTokenAddress);
+      const makerAmount = order.makerTokenAmount;
+      const takerAmount = order.takerTokenAmount;
+
+      const makerHowMuch = toReadable(config, makerAmount, makerSymbol);
+      const takerHowMuch = toReadable(config, takerAmount, takerSymbol);
+
+      return {
+        salt: order.salt,
+        maker: order.maker,
+        taker: order.taker,
+        isActive: true,
+        sell: {
+          symbol: makerSymbol,
+          howMuch: makerHowMuch,
+        },
+        buy: {
+          symbol: takerSymbol,
+          howMuch: takerHowMuch,
+        },
+        type: 'sell',
+        makerFee: order.makerFee,
+        takerFee: order.takerFee,
+        signature: order.ecSignature,
+        expiration: order.expirationUnixTimestampSec,
+        feeRecipient: order.feeRecipient,
+        exchangeContractAddress: order.exchangeContractAddress,
+        exchange,
+      }
+    }
+    catch (e) {
+      return undefined;
+    }
+  }).filter(order => typeof order !== 'undefined');
 
   const orderbook = [...formattedBids, ...formattedAsks].map(order => ({
     ...order,
