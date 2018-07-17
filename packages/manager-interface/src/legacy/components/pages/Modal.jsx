a2606f4d6ae38a736a73bd6be88d4b4f40ccae37
Jenna Zenk <zenkjenna@gmail.com>
2018-07-17 12:29:07 +0200

    Quick and dirty gas price field

--- packages/manager-interface/src/legacy/components/pages/Modal.jsx
+++ packages/manager-interface/src/legacy/components/pages/Modal.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {
-  Segment,
-  Container,
-  Card,
-  Header,
-  Loader,
-  Button,
-  Table,
+    Segment,
+    Container,
+    Card,
+    Header,
+    Loader,
+    Button,
+    Table,
 } from 'semantic-ui-react';
 import { Field } from 'redux-form';
 import ReactModal from 'react-modal';
@@ -15,190 +15,199 @@ import { add } from '../../utils/functionalBigNumber';
 import displayNumber from '../../utils/displayNumber';
 
 export const types = {
-  ERROR: 'ERROR',
-  LOADING: 'LOADING',
-  CONFIRM: 'CONFIRM',
-  PASSWORD: 'PASSWORD',
-  INFO: 'INFO',
+    ERROR: 'ERROR',
+    LOADING: 'LOADING',
+    CONFIRM: 'CONFIRM',
+    PASSWORD: 'PASSWORD',
+    INFO: 'INFO',
 };
 
 const style = {
-  overlay: {
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    backgroundColor: 'rgba(0, 0, 0, 0.8)',
-    zIndex: 200,
-  },
-  content: {
-    position: 'absolute',
-    top: '40px',
-    left: '40px',
-    right: '40px',
-    bottom: '40px',
-    border: 'none',
-    background: 'transparent',
-    overflow: 'auto',
-    WebkitOverflowScrolling: 'touch',
-    borderRadius: '4px',
-    outline: 'none',
-    padding: '20px',
-  },
+    overlay: {
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        backgroundColor: 'rgba(0, 0, 0, 0.8)',
+        zIndex: 200,
+    },
+    content: {
+        position: 'absolute',
+        top: '40px',
+        left: '40px',
+        right: '40px',
+        bottom: '40px',
+        border: 'none',
+        background: 'transparent',
+        overflow: 'auto',
+        WebkitOverflowScrolling: 'touch',
+        borderRadius: '4px',
+        outline: 'none',
+        padding: '20px',
+    },
 };
 
 const markError = {
-  color: 'rgb(201, 88, 88)',
+    color: 'rgb(201, 88, 88)',
 };
 
 const errorStyle = {
-  ...style,
-  content: {
-    ...style.content,
-    ...markError,
-  },
+    ...style,
+    content: {
+        ...style.content,
+        ...markError,
+    },
 };
 
 const renderButtons = (
-  primaryInteraction,
-  secondaryInteraction,
-  interactionHandler,
+    primaryInteraction,
+    secondaryInteraction,
+    interactionHandler,
 ) =>
-  secondaryInteraction ? (
-    <div>
-      <Button
-        style={{ width: '46%' }}
-        onClick={event => interactionHandler(event, primaryInteraction)}
-      >
-        {primaryInteraction}
-      </Button>
-      <Button
-        style={{ width: '46%', float: 'right' }}
-        onClick={event => interactionHandler(event, secondaryInteraction)}
-      >
-        {secondaryInteraction}
-      </Button>
-    </div>
-  ) : (
-    primaryInteraction && (
-      <Button
-        style={{ width: '100%' }}
-        onClick={event => interactionHandler(event, primaryInteraction)}
-      >
-        {primaryInteraction}
-      </Button>
-    )
-  );
+    secondaryInteraction ? (
+        <div>
+            <Button
+                style={{ width: '46%' }}
+                onClick={event => interactionHandler(event, primaryInteraction)}
+            >
+                {primaryInteraction}
+            </Button>
+            <Button
+                style={{ width: '46%', float: 'right' }}
+                onClick={event => interactionHandler(event, secondaryInteraction)}
+            >
+                {secondaryInteraction}
+            </Button>
+        </div>
+    ) : (
+            primaryInteraction && (
+                <Button
+                    style={{ width: '100%' }}
+                    onClick={event => interactionHandler(event, primaryInteraction)}
+                >
+                    {primaryInteraction}
+                </Button>
+            )
+        );
 
 const Modal = ({
   isOpen,
-  type,
-  title,
-  body,
-  fees,
-  method,
-  primaryInteraction,
-  secondaryInteraction,
-  interactionHandler,
-  handleSubmit,
-  onAfterOpen,
+    type,
+    title,
+    body,
+    fees,
+    method,
+    primaryInteraction,
+    secondaryInteraction,
+    interactionHandler,
+    handleSubmit,
+    onAfterOpen,
 }) => (
-  <ReactModal
-    onAfterOpen={onAfterOpen}
-    isOpen={isOpen}
-    style={type === types.ERROR ? errorStyle : style}
-  >
-    <Container>
-      <Card
-        centered
-        style={{ backgroundColor: '#fffdf3', padding: 10, width: 400 }}
-      >
-        <Card.Content>
-          {' '}
-          <form onSubmit={handleSubmit} data-hook="modal">
-            <Header as="h2" style={type === types.ERROR ? markError : {}}>
-              {title}
-            </Header>
+        <ReactModal
+            onAfterOpen={onAfterOpen}
+            isOpen={isOpen}
+            style={type === types.ERROR ? errorStyle : style}
+        >
+            <Container>
+                <Card
+                    centered
+                    style={{ backgroundColor: '#fffdf3', padding: 10, width: 400 }}
+                >
+                    <Card.Content>
+                        {' '}
+                        <form onSubmit={handleSubmit} data-hook="modal">
+                            <Header as="h2" style={type === types.ERROR ? markError : {}}>
+                                {title}
+                            </Header>
 
-            <p>{body}</p>
+                            <p>{body}</p>
 
-            {method ? (
-              <div>
-                <p>
-                  The following method on the Melon Smart Contracts will be
+                            {method ? (
+                                <div>
+                                    <p>
+                                        The following method on the Melon Smart Contracts will be
                   executed:
                 </p>
-                <h4>{method}</h4>
-              </div>
-            ) : null}
+                                    <h4>{method}</h4>
+                                </div>
+                            ) : null}
 
-            {fees ? (
-              <Table compact="very">
-                <Table.Header>
-                  <Table.Row>
-                    <Table.HeaderCell>Description</Table.HeaderCell>
-                    <Table.HeaderCell>Detail</Table.HeaderCell>
-                    <Table.HeaderCell style={{ textAlign: 'right' }}>
-                      Total
+                            {fees ? (
+                                <Table compact="very">
+                                    <Table.Header>
+                                        <Table.Row>
+                                            <Table.HeaderCell>Description</Table.HeaderCell>
+                                            <Table.HeaderCell>Gas Limit</Table.HeaderCell>
+                                            <Table.HeaderCell>Gas Price (Gwei)</Table.HeaderCell>
+                                            <Table.HeaderCell style={{ textAlign: 'right' }}>
+                                                Total
                     </Table.HeaderCell>
-                  </Table.Row>
-                </Table.Header>
-                <Table.Body>
-                  {fees.map((entry, i) => (
-                    <Table.Row key={`fee-{i}`}>
-                      <Table.Cell>{entry.description}</Table.Cell>
-                      <Table.Cell>{entry.detail}</Table.Cell>
-                      <Table.Cell style={{ textAlign: 'right' }}>
-                        Ξ {displayNumber(entry.inEth)}
-                      </Table.Cell>
-                    </Table.Row>
-                  ))}
-                </Table.Body>
-                <Table.Footer>
-                  <Table.Row>
-                    <Table.HeaderCell />
-                    <Table.HeaderCell />
-                    <Table.HeaderCell style={{ textAlign: 'right' }}>
-                      Ξ {displayNumber(add(...fees.map(e => e.inEth)))}
-                    </Table.HeaderCell>
-                  </Table.Row>
-                </Table.Footer>
-              </Table>
-            ) : null}
+                                        </Table.Row>
+                                    </Table.Header>
+                                    <Table.Body>
+                                        {fees.map((entry, i) => (
+                                            <Table.Row key={`fee-{i}`}>
+                                                <Table.Cell>{entry.description}</Table.Cell>
+                                                <Table.Cell>{entry.gasLimit}</Table.Cell>
+                                                <Table.Cell>
+                                                    <Field
+                                                        name="gasPrice"
+                                                        component={renderInput}
+                                                        type="text" placeholder={entry.gasPrice}
+                                                    />
+                                                </Table.Cell>
+                                                <Table.Cell style={{ textAlign: 'right' }}>
+                                                    Ξ {displayNumber(entry.inEth)}
+                                                </Table.Cell>
+                                            </Table.Row>
+                                        ))}
+                                    </Table.Body>
+                                    <Table.Footer>
+                                        <Table.Row>
+                                            <Table.HeaderCell />
+                                            <Table.HeaderCell />
+                                            <Table.HeaderCell />
+                                            <Table.HeaderCell style={{ textAlign: 'right' }}>
+                                                Ξ {displayNumber(add(...fees.map(e => e.inEth)))}
+                                            </Table.HeaderCell>
+                                        </Table.Row>
+                                    </Table.Footer>
+                                </Table>
+                            ) : null}
 
-            {type === types.PASSWORD ? (
-              <div style={{ marginBottom: 10 }}>
-                <Field
-                  name="password"
-                  component={renderInput}
-                  type="password"
-                />
-              </div>
-            ) : null}
+                            {type === types.PASSWORD ? (
+                                <div style={{ marginBottom: 10 }}>
+                                    <Field
+                                        name="password"
+                                        component={renderInput}
+                                        type="password"
+                                    />
+                                </div>
+                            ) : null}
 
-            {type !== types.LOADING ? (
-              renderButtons(
-                primaryInteraction,
-                secondaryInteraction,
-                interactionHandler,
-              )
-            ) : (
-              <Segment
-                style={{
-                  backgroundColor: 'transparent',
-                  border: 'none',
-                  boxShadow: 'none',
-                }}
-              >
-                <Loader active={type === types.LOADING} />
-              </Segment>
-            )}
-          </form>
-        </Card.Content>
-      </Card>
-    </Container>
-  </ReactModal>
-);
+                            {type !== types.LOADING ? (
+                                renderButtons(
+                                    primaryInteraction,
+                                    secondaryInteraction,
+                                    interactionHandler,
+                                )
+                            ) : (
+                                    <Segment
+                                        style={{
+                                            backgroundColor: 'transparent',
+                                            border: 'none',
+                                            boxShadow: 'none',
+                                        }}
+                                    >
+                                        <Loader active={type === types.LOADING} />
+                                    </Segment>
+                                )}
+                        </form>
+                    </Card.Content>
+                </Card>
+            </Container>
+        </ReactModal>
+    );
 
 export default Modal;
--- packages/melon.js/lib/utils/parity/sendTransaction.js
+++ packages/melon.js/lib/utils/parity/sendTransaction.js
@@ -2,101 +2,107 @@
 import BigNumber from 'bignumber.js';
 import gasBoost from '../ethereum/gasBoost';
 import constructTransactionObject from './constructTransactionObject';
+import getEnvironment from '../environment/getEnvironment'
 
 const sendTransaction = async (
+  contract,
+  method,
+  parameters,
+  environment,
+  opt = {},
+) => {
+  const nonce = environment.account.sign
+    ? (await environment.api.eth.getTransactionCount(
+      environment.account.address,
+    )).toNumber()
+    : undefined;
+
+  // Prepare raw transaction
+  const options = {
+    from: environment.account.address,
+    to: contract.address,
+    gasPrice: environment.gasPrice ? environment.gasPrice : 20000000000,
+    ...opt,
+  };
+
+  // HACK: If external parity signer, no need to set the nonce, Parity does it. If in-browser wallet, we need to explicitly set the nonce.
+  if (nonce) options.nonce = nonce;
+
+  // Estimate and adjust gas with gasBoost
+  const gasKeyName = environment.account.sign ? 'gasLimit' : 'gas';
+
+  if (
+    [
+      'cancelOrder',
+      'offer',
+      'depositStake',
+      'withdrawStake',
+      'collectAndUpdate',
+      'callOnExchange',
+    ].includes(method)
+  ) {
+    options[gasKeyName] = 6700000;
+  } else {
+    options[gasKeyName] = await gasBoost(
+      contract.instance[method],
+      parameters,
+      options,
+      environment,
+    );
+  }
+
+  if (options.value) {
+    options.value = `0x${options.value.toString(16)}`;
+  }
+
+  // Exit function prematurely if a confirmer is registered and it returns false
+  if (environment.confirmer) {
+    const confirmed = await environment.confirmer(method, [
+      {
+        description: 'Estimated max gas cost',
+        gasLimit: options[gasKeyName],
+        gasPrice: (options.gasPrice * 10 ** -9).toFixed(0),
+        inEth: new BigNumber(options[gasKeyName]).times(options.gasPrice).div(10 ** 18),
+      },
+    ]);
+
+    if (!confirmed) throw new Error('Transaction cancelled');
+    environment = await getEnvironment();
+
+    if (environment.gasPrice !== undefined) {
+      options.gasPrice = environment.gasPrice
+    }
+  }
+  // Construct raw transaction object
+  const rawTransaction = constructTransactionObject(
     contract,
     method,
     parameters,
-    environment,
-    opt = {},
-) => {
-    const nonce = environment.account.sign
-        ? (await environment.api.eth.getTransactionCount(
-            environment.account.address,
-        )).toNumber()
-        : undefined;
+    options,
+  );
 
-    // Prepare raw transaction
-    const options = {
-        from: environment.account.address,
-        to: contract.address,
-        gasPrice: 30000000000,
-        ...opt,
-    };
-
-    // HACK: If external parity signer, no need to set the nonce, Parity does it. If in-browser wallet, we need to explicitly set the nonce.
-    if (nonce) options.nonce = nonce;
-
-    // Estimate and adjust gas with gasBoost
-    const gasKeyName = environment.account.sign ? 'gasLimit' : 'gas';
-
-    if (
-        [
-            'cancelOrder',
-            'offer',
-            'depositStake',
-            'withdrawStake',
-            'collectAndUpdate',
-            'callOnExchange',
-        ].includes(method)
-    ) {
-        options[gasKeyName] = 6700000;
-    } else {
-        options[gasKeyName] = await gasBoost(
-            contract.instance[method],
-            parameters,
-            options,
-            environment,
-        );
-    }
-
-    if (options.value) {
-        options.value = `0x${options.value.toString(16)}`;
-    }
-
-    // Exit function prematurely if a confirmer is registered and it returns false
-    if (environment.confirmer) {
-        const confirmed = await environment.confirmer(method, [
-            {
-                description: 'Estimated max gas cost',
-                detail: `${options[gasKeyName]} (Gas Limit) × 30 Gwei (Gas Price)`,
-                inEth: new BigNumber(options[gasKeyName]).times(30).div(1000000000),
-            },
-        ]);
-
-        if (!confirmed) throw new Error('Transaction cancelled');
-    }
-
-    // Construct raw transaction object
-    const rawTransaction = constructTransactionObject(
-        contract,
-        method,
-        parameters,
-        options,
+  let transactionHash;
+  if (environment.account.sign) {
+    // Sign transaction object with Wallet instance
+    const signedTransaction = environment.account.sign(rawTransaction);
+    // Send raw signed transaction and wait for receipt
+    transactionHash = await environment.api.eth.sendRawTransaction(
+      signedTransaction,
     );
+  } else {
+    // Send raw transaction object and wait for receipt
+    transactionHash = await environment.api.eth.sendTransaction(rawTransaction);
+  }
 
-    let transactionHash;
-    if (environment.account.sign) {
-        // Sign transaction object with Wallet instance
-        const signedTransaction = environment.account.sign(rawTransaction);
-        // Send raw signed transaction and wait for receipt
-        transactionHash = await environment.api.eth.sendRawTransaction(
-            signedTransaction,
-        );
-    } else {
-        // Send raw transaction object and wait for receipt
-        transactionHash = await environment.api.eth.sendTransaction(rawTransaction);
-    }
+  // eslint-disable-next-line no-underscore-dangle
+  await contract._pollTransactionReceipt(transactionHash);
 
-    // eslint-disable-next-line no-underscore-dangle
-    await contract._pollTransactionReceipt(transactionHash);
-
-    const rawReceipt = await environment.api.eth.getTransactionReceipt(
-        transactionHash,
-    );
-    const decodedLogs = contract.parseEventLogs(rawReceipt.logs);
-    const transactionReceipt = { ...rawReceipt, logs: decodedLogs };
-    return transactionReceipt;
+  const rawReceipt = await environment.api.eth.getTransactionReceipt(
+    transactionHash,
+  );
+  const decodedLogs = contract.parseEventLogs(rawReceipt.logs);
+  const transactionReceipt = { ...rawReceipt, logs: decodedLogs };
+  return transactionReceipt;
 };
 
 export default sendTransaction;
