ed873b1557e534870660aa12ab6673e48e99b5a6
Jenna Zenk <zenkjenna@gmail.com>
2018-07-19 21:03:38 +0200

    Update OlympiadPlaceholder.jsx

--- packages/manager-interface/src/legacy/components/pages/OlympiadPlaceholder.jsx
+++ packages/manager-interface/src/legacy/components/pages/OlympiadPlaceholder.jsx
@@ -7,18 +7,18 @@ import GetStarted from '../../containers/GetStarted';
 import renderInput from '../utils/renderInput';
 
 const OlympiadPlaceholder = ({ goToGenerateAccount, goToAccount, address }) => (
-  <div style={{ maxWidth: '35em', margin: '0 auto' }}>
+  <div style={{ maxWidth: '70em', margin: '0 auto' }}>
     <h1 id="history" className="App-intro">
       Welcome to the Melon Olympiad
     </h1>
-    <h5>
+    {/*<h5>
       <a
         href="https://medium.com/@zenkjenna/introducing-melon-olympiad-861687d0703b"
         target="_blank"
       >
         [Click here to read blog post introducing the Melon Olympiad]
       </a>
-    </h5>
+    </h5>*/}
     <p>
       <i>([méllō], μέλλω; Greek for “destined to be”)</i> is blockchain software
       that seeks to enable participants to set up, manage and invest in
@@ -38,113 +38,142 @@ const OlympiadPlaceholder = ({ goToGenerateAccount, goToAccount, address }) => (
         competitions composed of several rounds. Each of the rounds will set a
         clear goal for participants, and will issue a defined number of Melon
         tokens.
-      </strong>{' '}
-      At the end of the Melon Olympiad, all remaining Melon tokens shall have
-      been issued to the public. {' '}
+      </strong>
     </p>
     <p>
-      <i>
-        {' '}
-        Each round of the Melon Olympiad will be named after Greek islands. The
-        first round of the Melon Olympiad is referred to as Paros.
-      </i>
+      Following a successful token sale in 2017, Melonport AG is left with
+      500,600 Melon (MLN) tokens to distribute. Rather than distribute these in
+      a traditional manner, Melonport has created a series of sub-contribution
+      periods called Melon Olympiad. This will ensure that tokens are allocated
+      to active community members who engage and use the Melon asset management
+      ecosystem.{' '}
+    </p>{' '}
+    <p>
+      The distribution of up to half a million Melon tokens will largely occur
+      via a public contribution period is in keeping with Melonport’s commitment
+      to creating a diverse and decentralized ecosystem in which there are as
+      many participants as possible. By awarding tokens to testers who have put
+      their time and energy into scrutinizing the Melon software, it will also
+      ensure adoption and help to further improve the codebase.{' '}
+    </p>{' '}
+    <p>
+      The contribution period will be split into rounds referred to as Melon
+      Olympiad capsules, with a portion of tokens allocated for each stage. Upon
+      completion of the Melon Olympiad, all 500,600 tokens will have been
+      issued. In keeping with the mythology and provenance of the Olympic Games,
+      each round will be named after a Greek island, commencing with Paros.
+      Participants will be obliged to create a fund and send Ether to the
+      relevant Olympiad smart-contract, which will then be matched with an
+      equivalent amount of MLN tokens plus an additional incentive amount. It is
+      then up to the participant to manage the fund for a period of 2 weeks with
+      a view to maximizing capital appreciation.{' '}
+    </p>{' '}
+    <p>
+      The Melon Protocol intends to present a decentralized, public,
+      permissionless, robust infrastructure for the secure management of digital
+      assets on the Ethereum blockchain. It aims to be a viable, low-cost
+      alternative to the current fund management ecosystem which has evolved
+      similarly across most legal jurisdictions. The protocol itself is a
+      collection of smart-contracts written in the Solidity programming
+      language. Supporting functionality that allows browsers to freely interact
+      with the protocol is provided by the javascript library Melon.js. For more
+      information visit: https://docs.melonport.com and our github
+      https://github.com/melonproject/.{' '}
+    </p>{' '}
+    <p>
+      Participants interested in taking part in the Melon Olympiad Paros can
+      register by following the instructions provided below.
     </p>
     <h2>Melon Olympiad, first round: Paros</h2>
     <p>
-      Participants who wish to engage in testing and help us improve the
-      software will be rewarded for this effort. Participants will have to
-      create a fund on this website and contribute ether to the Olympiad
-      contract. In return, the Olympiad contract will invest the equivalent
-      amount in MLN (+ a bonus) into the participant’s fund. The participant
-      will have to manage his fund for a defined period of time (2 weeks) with
-      the goal being capital appreciation. At the end of the defined management
-      period, the participant will be able to trigger the redemption of the
-      fund’s assets, thus receiving the fund’s AUM as a reward for his effort.{' '}
-    </p>
-    <p>
       For the Paros round, the participant compensation is as follows: for each
-      ether contributed, the participant will receive the equivalent of 2 ethers
-      in Melon tokens. The conversion rate of ether to Melon will be retrieved
-      from the Pricefeed contract.
+      ether contributed, the participant will receive 20 MLN (subject to change
+      prior to the start of the Paros Olympiad in the event of price
+      volatility).
     </p>
     <h2>Instructions on how to participate</h2>
     <p>
-      Melonport AG is required to issue tokens via a regulated financial
-      intermediary which is subject to KYC/AML procedures. To perform the
-      KYC/AML processes, our chosen partner is Bitcoin Suisse. Each individual
-      willing to participate will have to register with Bitcoin Suisse.
+      Paros is open to all participants who have undergone the KYC process with Bitcoin Suisse and whitelisted their Ethereum addresses generated on olympiad.melon.fund.
+    </p>
+    <p>
+      In order to ensure the highest level of security and performance, we strongly recommend you to download the Electron app and to run your own parity node. You can find the instructions <a
+        href="https://www.youtube.com/watch?v=Wp5DAFWfc8U&t=25s"
+        target="_blank"
+      >here</a>.
+    </p>
+    <p>
+      Before we start, make sure you have either one of the two options ready.
+    </p>
+    <p>
+      1. Your mnemonic phrase (12 words seed phrase) or
+    </p>
+    <p>
+      2. Your JSON file + password
+    </p>
+    <p>
+      In order to contribute ETH to Paros you need to first fund your wallet with the desired amount you want to contribute. In addition, you need to have a bit more ETH in your wallet to cover the gas costs for setting up your fund, register in the competition and (if applicable) to trade.
     </p>
     <h4>Step 1</h4> <p>
-      Generate a wallet on this website (see instructins below).. Make sure your
-      write down the mnemonic, and store it in a safe (and accessible) place.
-      Make sure you remember the password you use to encrypt the wallet. If you
-      forget your password and you loose access to the mnemonic phrase, you
-      won't be able to participate in the challenge. Make sure you download your
-      wallet as a JSON file and store it in a safe place. Save the wallet
-      address as you will need to provide this address to Bitcoin Suisse in step
-      3.
+      Press "Setup Fund"
     </p>
     <h4>Step 2 </h4> <p>
-      KYC with Bitcoin Suisse: create an account with Bitcoin Suisse. Follow the
-      steps on the Bitcoin Suisse platform to get the KYC clearance. Make sure
-      you upload all required documents. Bitcoin Suisse will inform you with an
-      email once your KYC is approved. Once it is approved, you can move on to
-      Step 3.{' '}
+      Import your wallet either by importing your mnemonic phrase or by uploading your JSON file and decrypting it with your password. You can see your public address on the top right of the page, together with your ETH balance. Your wallet needs to be the whitelisted wallet on Bitcoin Suisse, otherwise you won’t be able to create a fund. In terms of the Melon protocol, this wallet will act as the owner wallet of your fund (manager address), which you will create in a minute.
     </p>
     <h4>Step 3 </h4> <p>
-       Apply for the Melon Olympiad Whitelist on the Bitcoin Suisse platform.
-      You will need to provide an Ethereum address (the one created in Step 1)
-      and the amount you wish to contribute with. It is of utmost importance
-      that you provide an Ethereum address that was generated on this website.
-      Bitcoin Suisse will inform you once your address is whitelisted. They will
-      also inform you of the maximum amount you can contribute with.
+      Read and agree to the <a
+        href="https://github.com/melonproject/contribution/blob/master/Melonport_Second%20Contribution%20Period_FINAL.pdf"
+        target="_blank"
+      >terms and conditions</a>.
     </p>
-    <h4>Step 4  </h4>
+    <h4>Step 4  </h4> <p>
+      Choose a name for your fund and press “Setup fund”.
+    </p>
+    <h4>Step 5 </h4>{' '}
     <p>
-       Get ready before the start of the competition. Fund your wallet
-      (generated in Step 1) with the amount of Ether you would like to
-      contribute with. After this point, you need to wait until the official
-      beginning of the challenge which will be announced shortly.
+      You are now at the <strong>contribution page</strong>. Put in the amount you want to contribute to the Paros smart contract. Below, you will see the estimated amount of MLN tokens that will be invested into your fund by the Paros smart contract (the conversion rate is fixed at 20 MLN tokens for 1 ETH). Make sure that you have enough ETH to cover the gas costs. Once you are ready, please press the “Contribute” button to send ETH to the Paros smart contract. <strong>The contract, in turn, will invest the equivalent amount in MLN to your fund.</strong>
     </p>
-    <h4>Step 5 </h4> <p> Official start of the Melon Olympiad, PAROS.</p>
     <h4>Step 6  </h4>{' '}
     <p>
-      Come back to this website olympiad.melon.fund. You will be guided to
-      create your fund. After creating your fund, you will be asked to enter the
-      amount you wish to contribute with. Once you confirm that transaction, the
-      amount of Ether will be transferred from you wallet to the Olympiad
-      contract. In return, the Olympiad contract will invest the equivalent
-      amount in MLN into your fund.
+      Come back to this website olympiad.melon.fund. Once Paros is live, you
+      will be guided to create your fund.
     </p>
     <h4>Step 7  </h4>{' '}
     <p>
-      After creating your fund, you will be asked to enter the amount you wish
-      to contribute with. Once you confirm that transaction, the amount of Ether
-      will be transferred from your wallet to the Olympiad contract. In return,
-      the Olympiad contract will invest the equivalent amount in MLN into your
-      fund.
-    </p>
-    <h4>Step 8 </h4>{' '}
-    <p>
-      Congratulations. You made it here. Now its time to prove your asset
-      management skill. You have 2 weeks to manage your fund and grow your
-      capital.
-    </p>
-    <h4>Step 9  </h4>{' '}
-    <p>
-       At the end of the round, and only at the end, you, and only you, will
-      have the option on your fund’s page to trigger the redemption of the
-      shares and therefore to retrieve your earned assets using the wallet you
-      generated in step 1.
+      Your fund is now deployed to the Ethereum main net and funded with MLN tokens. You can see the public address of your fund in the URL.
     </p>
     <p>
-      The terms and conditions are being finalized and will be published on this
-      page as soon as ready. Please make sure you read carefully the terms and
-      conditions. When you will create a fund, you will be prompted to
-      cryptographically sign those terms and conditions to show your
-      understanding and agreement.
+      From this point onwards, you are free to do what you want. You can see the basic information about your fund in the middle. Your fund is denominated in WETH. There are two options:
     </p>
-    <h2>Generate your wallet</h2>
+    <p>
+        1. <strong>Keep the MLN tokens</strong>. In this case, you do not need to do anything until Paros ends (you can see the official end date/time in the top right corner). Once Paros has ended, you will have the option to claim your reward and redeem your shares, which will transfer all MLN tokens of the fund to your wallet address.
+    </p>
+    <p>
+        <strong>OR</strong>
+    </p>
+    <p>
+        2. <strong>Trade</strong>. If you would like to test your fund management skills, you can do so by trading on the decentralized exchanges we have integrated (OasisDex, Radar Relay, ERCDex). As most assets are quoted against ETH, the first trade you need to place is to sell some of your MLN tokens against WETH.
+    </p>
+    <p>
+      The terms and conditions must be read by all participants.{' '}
+      <a
+        href="https://ipfs.io/ipfs/QmbTsKQgACY9DzaW3SVSnLuCTo6f4H24SiXXfAyAGrTYTz"
+        target="_blank"
+      >
+        Click here to read the terms and conditions for the second contribution
+        period{' '}
+      </a>.{' '}
+      <a
+        href="https://ipfs.io/ipfs/Qme4yLq3vFD2hiSyyJR3oukQYWMR9vBsducrNipWXLFnug"
+        target="_blank"
+      >
+        {' '}
+        Click here to read the terms and conditions for the Paros Olympiad
+        (first capsule).{' '}
+      </a>Please make sure you read carefully both documents. When you will
+      create a fund, you will be prompted to cryptographically sign those terms
+      and conditions to show your understanding and agreement.
+    </p>
+    {/*<h2>Generate your wallet</h2>
     {!address ? (
       <div>
         <p>
@@ -152,8 +181,7 @@ const OlympiadPlaceholder = ({ goToGenerateAccount, goToAccount, address }) => (
           a fund when the competition starts (referred to as manager address).
           It will be the wallet you will use to contribute ether to the Olympiad
           contract. It is also the wallet to which your compensation will be
-          transferred to at the end of the period. Please make sure you
-          carefully read the warnings below.
+          transferred to at the end of the period.
         </p>
         <p>
           The first step to perform if you wish to participate is to generate
@@ -201,7 +229,7 @@ const OlympiadPlaceholder = ({ goToGenerateAccount, goToAccount, address }) => (
           </Button>
         </p>
       </div>
-    )}
+    )}*/}
   </div>
 );
 
