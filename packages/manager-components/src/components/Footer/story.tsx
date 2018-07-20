3748598cdb71a7d355e95b0f8d26f9f9313ce677
Matthias Gemperli <hello@matthiasgemperli.ch>
2018-07-20 08:49:00 +0200

    Add footer

---
+++ packages/manager-components/src/components/Footer/story.tsx
@@ -0,0 +1,7 @@
+import { storiesOf } from '@storybook/react';
+import React from 'react';
+import Footer from './index';
+
+storiesOf('Components|Footer', module).add('Default', () => {
+  return <Footer />;
+});
