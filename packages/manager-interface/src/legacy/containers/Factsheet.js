a700537096bfa828a7bf9e7fcc5294849bd0fd32
Jenna Zenk <zenkjenna@gmail.com>
2018-07-25 14:48:57 +0200

    Fix twitter links and text

--- packages/manager-interface/src/legacy/containers/Factsheet.js
+++ packages/manager-interface/src/legacy/containers/Factsheet.js
@@ -7,62 +7,67 @@ import displayNumber from "../utils/displayNumber";
 import { actions as appActions } from "../actions/app";
 import { actions as administrationActions } from "../actions/administration";
 
-const buildTwitterUrl = (isOwner, fundAddress, fundName, sharePrice) => {
-  const text = isOwner
-    ? `My #MelonFund "${fundName}" has a share price currently of ${displayNumber(
-      sharePrice,
-    )}. Have a look:`
-    : `The #MelonFund "${fundName}" has a share price currently of ${displayNumber(
-      sharePrice,
-    )}. Have a look:`;
+const buildTwitterUrl = (isOwner, fundAddress, fundName, sharePrice, track) => {
+    const text = isOwner
+        ? track !== "live" ? `My #MelonFund "${fundName}" has a share price currently of ${displayNumber(
+            sharePrice,
+        )}. Have a look:` : `Check out my on-chain decentralized hedge fund "${fundName}". It currently has a share price of ${displayNumber(
+            sharePrice,
+        )}. Have a look:`
+        : track !== "live" ? `The #MelonFund "${fundName}" has a share price currently of ${displayNumber(
+            sharePrice,
+        )}. Have a look:` : `Check out this on-chain decentralized hedge fund "${fundName}". It currently has a share price of ${displayNumber(
+            sharePrice,
+        )}. Have a look:`;
 
-  const url = `https://ipfs.io/ipns/melon.fund/#${fundAddress}`;
-  const hashtags = "TechnologyRegulatedFunds,Melon";
-  const via = "melonport";
-  const related = "melonport";
+    const url = track === "live" ? `https://ipfs.io/ipns/olympiad.melon.fund/#${fundAddress}` : `https://ipfs.io/ipns/melon.fund/#${fundAddress}`;
+    const hashtags = "TechnologyRegulatedFunds,Melon,MelonFund";
+    const via = "melonport";
+    const related = "melonport";
 
-  return `https://twitter.com/intent/tweet?text=${encodeURIComponent(
-    text,
-  )}&url=${encodeURIComponent(url)}&hashtags=${encodeURIComponent(
-    hashtags,
-  )}&via=${encodeURIComponent(via)}&related=${encodeURIComponent(related)}`;
+    return `https://twitter.com/intent/tweet?text=${encodeURIComponent(
+        text,
+    )}&url=${encodeURIComponent(url)}&hashtags=${encodeURIComponent(
+        hashtags,
+    )}&via=${encodeURIComponent(via)}&related=${encodeURIComponent(related)}`;
 };
 
 const mapStateToProps = state => ({
-  aum: displayNumber(state.fund.gav),
-  creationDate:
-  state.fund.inception && state.fund.inception !== "..."
-    ? moment(state.fund.inception).format("D. MMM YYYY HH:mm")
-    : "...",
-  managementReward: displayNumber(state.fund.managementReward),
-  name: state.fund.name,
-  performanceReward: displayNumber(state.fund.performanceReward),
-  personalStake: displayNumber(state.fund.personalStake),
-  sharePrice: displayNumber(state.fund.sharePrice),
-  totalSupply: displayNumber(state.fund.totalSupply),
-  rank: state.fund.rank,
-  dataValid: state.ethereum.isDataValid,
-  numberOfFunds: state.fund.numberOfFunds,
-  tweetHref: buildTwitterUrl(
-    state.fund.owner === state.ethereum.account,
-    state.fund.address,
-    state.fund.name,
-    state.fund.sharePrice,
-  ),
-  loading: state.fund.loading,
-  expectedPrize: displayNumber(state.fund.expectedPrize),
-  quoteAsset: state.app.assetPair.quote,
-  isCompetition: state.app.isCompetition,
-
+    aum: displayNumber(state.fund.gav),
+    creationDate:
+    state.fund.inception && state.fund.inception !== "..."
+        ? moment(state.fund.inception).format("D. MMM YYYY HH:mm")
+        : "...",
+    managementReward: displayNumber(state.fund.managementReward),
+    name: state.fund.name,
+    performanceReward: displayNumber(state.fund.performanceReward),
+    personalStake: displayNumber(state.fund.personalStake),
+    sharePrice: displayNumber(state.fund.sharePrice),
+    totalSupply: displayNumber(state.fund.totalSupply),
+    rank: state.fund.rank,
+    dataValid: state.ethereum.isDataValid,
+    numberOfFunds: state.fund.numberOfFunds,
+    tweetHref: buildTwitterUrl(
+        state.fund.owner === state.ethereum.account,
+        state.fund.address,
+        state.fund.name,
+        state.fund.sharePrice,
+        state.app.track
+    ),
+    loading: state.fund.loading,
+    expectedPrize: displayNumber(state.fund.expectedPrize),
+    quoteAsset: state.app.assetPair.quote,
+    isCompetition: state.app.isCompetition,
+    track: state.app.track
 });
 
 const mapDispatchToProps = dispatch => ({
-  scrollTo: target => dispatch(appActions.scrollTo(target)),
-  shutdown: () => dispatch(administrationActions.shutdown()),
+    scrollTo: target => dispatch(appActions.scrollTo(target)),
+    shutdown: () => dispatch(administrationActions.shutdown()),
 });
 
 const FactsheetContainer = connect(mapStateToProps, mapDispatchToProps)(
-  Factsheet,
+    Factsheet,
 );
 
 export default FactsheetContainer;
