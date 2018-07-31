d4af74f958ff07aa8c431c4bdaa145831d9d52ee
Sebastian Siemssen <fubhy@fubhy.com>
2018-07-31 15:23:21 +0200

    Build app.

--- packages/manager-interface/src/pages/index.tsx
+++ packages/manager-interface/src/pages/index.tsx
@@ -1,12 +1,3 @@
-import dynamic from 'next/dynamic';
-import withApollo from '~/shared/graphql';
-
 import legacy from '../legacy';
 
-// console.log(legacy);
-
-const App = dynamic(import('../legacy/index'), {
-  ssr: false,
-});
-
-export default withApollo(App);
+export default legacy;
