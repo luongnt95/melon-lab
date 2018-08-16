8449f79268cf12c574fa9c4266b08e22290cbef5
Matthias Gemperli <hello@matthiasgemperli.ch>
2018-08-16 17:06:56 +0200

    Clean up

--- packages/manager-interface/src/legacy/components/pages/Fund.jsx
+++ packages/manager-interface/src/legacy/components/pages/Fund.jsx
@@ -1,8 +1,5 @@
 import React, { Fragment } from 'react';
-import { Card } from 'semantic-ui-react';
-
 import { tracks } from '@melonproject/melon.js';
-
 import Factsheet from '../../containers/Factsheet';
 import Holdings from '../../containers/Holdings';
 import Administration from '../../containers/Administration';
