12a06fa18f9f9ff6d16e225954a9a1d0a69a6436
Jenna Zenk <zenkjenna@gmail.com>
2018-08-17 11:12:43 +0200

    Fix volume on ask side

--- packages/manager-components/src/components/Orderbook/index.tsx
+++ packages/manager-components/src/components/Orderbook/index.tsx
@@ -81,160 +81,160 @@ export const Orderbook: StatelessComponent<OrderbookProps> = ({
           <Spinner />
         </div>
       ) : (
-        <Fragment>
-          {orderbook &&
-          orderbook.sellEntries.length === 0 &&
-          orderbook.buyEntries.length === 0 ? (
-            <Notification isWarning>
-              No orders on the orderbook for this trading pair
+          <Fragment>
+            {orderbook &&
+              orderbook.sellEntries.length === 0 &&
+              orderbook.buyEntries.length === 0 ? (
+                <Notification isWarning>
+                  No orders on the orderbook for this trading pair
             </Notification>
-          ) : (
-            <div className="orderbook__tables">
-              <div className="orderbook__table orderbook__table-buy">
-                <div>
-                  <div className="orderbook__head">
-                    <div className="orderbook__head-row">
-                      <div className="orderbook__head-cell">Cum. Vol.</div>
-                      <div className="orderbook__head-cell">Vol.</div>
-                      <div className="orderbook__head-cell">Bid</div>
+              ) : (
+                <div className="orderbook__tables">
+                  <div className="orderbook__table orderbook__table-buy">
+                    <div>
+                      <div className="orderbook__head">
+                        <div className="orderbook__head-row">
+                          <div className="orderbook__head-cell">Cum. Vol.</div>
+                          <div className="orderbook__head-cell">Vol.</div>
+                          <div className="orderbook__head-cell">Bid</div>
+                        </div>
+                      </div>
+                      <div className="orderbook__body">
+                        {orderbook &&
+                          orderbook.buyEntries.map((entry, index) => {
+                            const volume = Number.parseFloat(entry.volume).toFixed(
+                              decimals,
+                            );
+                            const howMuch = Number.parseFloat(
+                              entry.order.buy.howMuch,
+                            ).toFixed(decimals);
+                            const price = Number.parseFloat(
+                              entry.order.price,
+                            ).toFixed(decimals);
+
+                            const onClickBuyOrder = () => {
+                              if (isReadyToTrade && onClick) {
+                                onClick(orderbook.buyEntries.slice(0, index + 1));
+                              }
+                            };
+
+                            const calculatedBar = calculateBar(
+                              orderbook.buyEntries[index - 1],
+                              entry,
+                            );
+
+                            return (
+                              <div
+                                className="orderbook__body-row"
+                                key={entry.order.id}
+                                onClick={onClickBuyOrder}
+                                style={{
+                                  cursor: isReadyToTrade ? 'pointer' : 'auto',
+                                }}
+                              >
+                                <div className="orderbook__body-cell">{volume}</div>
+                                <div className="orderbook__body-cell">
+                                  {howMuch}
+                                </div>
+                                <div className="orderbook__body-cell orderbook__body-cell--buy">
+                                  {price}
+                                </div>
+                                <div className="orderbook__bar orderbook__bar--buy">
+                                  <Bar
+                                    widthBar={`${
+                                      calculatedBar.entryPercentage > 100
+                                        ? 100
+                                        : calculatedBar.entryPercentage
+                                      }%`}
+                                    widthBorder={`${calculatedBar.percentageDiff}%`}
+                                    leftSpaceBorder={`calc(100% - ${
+                                      calculatedBar.entryPercentage
+                                      }% ${calculatedBar.prevEntryPercentage > 0.5 &&
+                                      '+ 1px'})`}
+                                  />
+                                </div>
+                              </div>
+                            );
+                          })}
+                      </div>
                     </div>
                   </div>
-                  <div className="orderbook__body">
-                    {orderbook &&
-                      orderbook.buyEntries.map((entry, index) => {
-                        const volume = Number.parseFloat(entry.volume).toFixed(
-                          decimals,
-                        );
-                        const howMuch = Number.parseFloat(
-                          entry.order.buy.howMuch,
-                        ).toFixed(decimals);
-                        const price = Number.parseFloat(
-                          entry.order.price,
-                        ).toFixed(decimals);
+                  <div className="orderbook__table orderbook__table-sell">
+                    <div>
+                      <div className="orderbook__head">
+                        <div className="orderbook__head-row">
+                          <div className="orderbook__head-cell">Ask</div>
+                          <div className="orderbook__head-cell">Vol.</div>
+                          <div className="orderbook__head-cell">Cum. Vol.</div>
+                        </div>
+                      </div>
+                      <div className="orderbook__body">
+                        {orderbook &&
+                          orderbook.sellEntries.map((entry, index) => {
+                            const volume = Number.parseFloat(entry.volume).toFixed(
+                              decimals,
+                            );
+                            const howMuch = Number.parseFloat(
+                              entry.order.sell.howMuch,
+                            ).toFixed(decimals);
+                            const price = Number.parseFloat(
+                              entry.order.price,
+                            ).toFixed(decimals);
 
-                        const onClickBuyOrder = () => {
-                          if (isReadyToTrade && onClick) {
-                            onClick(orderbook.buyEntries.slice(0, index + 1));
-                          }
-                        };
+                            const onClickSellOrder = () => {
+                              if (isReadyToTrade && onClick) {
+                                onClick(orderbook.sellEntries.slice(0, index + 1));
+                              }
+                            };
 
-                        const calculatedBar = calculateBar(
-                          orderbook.buyEntries[index - 1],
-                          entry,
-                        );
+                            const calculatedBar = calculateBar(
+                              orderbook.sellEntries[index - 1],
+                              entry,
+                            );
 
-                        return (
-                          <div
-                            className="orderbook__body-row"
-                            key={entry.order.id}
-                            onClick={onClickBuyOrder}
-                            style={{
-                              cursor: isReadyToTrade ? 'pointer' : 'auto',
-                            }}
-                          >
-                            <div className="orderbook__body-cell">{volume}</div>
-                            <div className="orderbook__body-cell">
-                              {howMuch}
-                            </div>
-                            <div className="orderbook__body-cell orderbook__body-cell--buy">
-                              {price}
-                            </div>
-                            <div className="orderbook__bar orderbook__bar--buy">
-                              <Bar
-                                widthBar={`${
-                                  calculatedBar.entryPercentage > 100
-                                    ? 100
-                                    : calculatedBar.entryPercentage
-                                }%`}
-                                widthBorder={`${calculatedBar.percentageDiff}%`}
-                                leftSpaceBorder={`calc(100% - ${
-                                  calculatedBar.entryPercentage
-                                }% ${calculatedBar.prevEntryPercentage > 0.5 &&
-                                  '+ 1px'})`}
-                              />
-                            </div>
-                          </div>
-                        );
-                      })}
-                  </div>
-                </div>
-              </div>
-              <div className="orderbook__table orderbook__table-sell">
-                <div>
-                  <div className="orderbook__head">
-                    <div className="orderbook__head-row">
-                      <div className="orderbook__head-cell">Ask</div>
-                      <div className="orderbook__head-cell">Vol.</div>
-                      <div className="orderbook__head-cell">Cum. Vol.</div>
+                            return (
+                              <div
+                                className="orderbook__body-row"
+                                key={entry.order.id}
+                                onClick={onClickSellOrder}
+                                style={{
+                                  cursor: isReadyToTrade ? 'pointer' : 'auto',
+                                }}
+                              >
+                                <div className="orderbook__body-cell orderbook__body-cell--sell">
+                                  {price}
+                                </div>
+                                <div className="orderbook__body-cell">
+                                  {howMuch}
+                                </div>
+                                <div className="orderbook__body-cell">{volume}</div>
+
+                                <div className="orderbook__bar orderbook__bar--sell">
+                                  <Bar
+                                    widthBar={`${
+                                      calculatedBar.entryPercentage > 100
+                                        ? 100
+                                        : calculatedBar.entryPercentage
+                                      }%`}
+                                    widthBorder={`${calculatedBar.percentageDiff}%`}
+                                    leftSpaceBorder={`calc(${
+                                      calculatedBar.entryPercentage
+                                      }% - ${
+                                      calculatedBar.percentageDiff
+                                      }% ${calculatedBar.prevEntryPercentage > 0.5 &&
+                                      '- 1px'})`}
+                                  />
+                                </div>
+                              </div>
+                            );
+                          })}
+                      </div>
                     </div>
                   </div>
-                  <div className="orderbook__body">
-                    {orderbook &&
-                      orderbook.sellEntries.map((entry, index) => {
-                        const volume = Number.parseFloat(entry.volume).toFixed(
-                          decimals,
-                        );
-                        const howMuch = Number.parseFloat(
-                          entry.order.buy.howMuch,
-                        ).toFixed(decimals);
-                        const price = Number.parseFloat(
-                          entry.order.price,
-                        ).toFixed(decimals);
-
-                        const onClickSellOrder = () => {
-                          if (isReadyToTrade && onClick) {
-                            onClick(orderbook.sellEntries.slice(0, index + 1));
-                          }
-                        };
-
-                        const calculatedBar = calculateBar(
-                          orderbook.sellEntries[index - 1],
-                          entry,
-                        );
-
-                        return (
-                          <div
-                            className="orderbook__body-row"
-                            key={entry.order.id}
-                            onClick={onClickSellOrder}
-                            style={{
-                              cursor: isReadyToTrade ? 'pointer' : 'auto',
-                            }}
-                          >
-                            <div className="orderbook__body-cell orderbook__body-cell--sell">
-                              {price}
-                            </div>
-                            <div className="orderbook__body-cell">
-                              {howMuch}
-                            </div>
-                            <div className="orderbook__body-cell">{volume}</div>
-
-                            <div className="orderbook__bar orderbook__bar--sell">
-                              <Bar
-                                widthBar={`${
-                                  calculatedBar.entryPercentage > 100
-                                    ? 100
-                                    : calculatedBar.entryPercentage
-                                }%`}
-                                widthBorder={`${calculatedBar.percentageDiff}%`}
-                                leftSpaceBorder={`calc(${
-                                  calculatedBar.entryPercentage
-                                }% - ${
-                                  calculatedBar.percentageDiff
-                                }% ${calculatedBar.prevEntryPercentage > 0.5 &&
-                                  '- 1px'})`}
-                              />
-                            </div>
-                          </div>
-                        );
-                      })}
-                  </div>
                 </div>
-              </div>
-            </div>
-          )}
-        </Fragment>
-      )}
+              )}
+          </Fragment>
+        )}
     </div>
   );
 };
