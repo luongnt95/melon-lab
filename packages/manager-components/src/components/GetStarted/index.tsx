01e33776859993f33da932a93b27de5d50123093
Matthias Gemperli <hello@matthiasgemperli.ch>
2018-08-21 16:23:39 +0200

    Add title to home

--- packages/manager-components/src/components/GetStarted/index.tsx
+++ packages/manager-components/src/components/GetStarted/index.tsx
@@ -27,35 +27,45 @@ export const GetStarted: StatelessComponent<GetStartedProps> = ({
   return (
     <div className="get-started">
       <style jsx>{styles}</style>
-      <h3>Get started</h3>
-      {networkId === '1' ? (
-        <Fragment>
-          <h4> You are accessing Melon on the Ethereum main network.</h4>
-          <p>
-            AT THE END OF THE TESTING PERIOD OR AT ANY ARBITRARY POINT IN TIME
-            AS DEFINED BY MELONPORT AG, THE SMART CONTRACT SYSTEM WILL BE CLOSED
-            DOWN. THIS IS EXPECTED TO HAPPEN IN A MATTER OF DAYS OR WEEKS FROM
-            DEPLOYMENT DATE. AT THIS POINT, THE USERS WON’T BE ABLE TO INTERACT
-            WITH THE THE SMART CONTRACT SYSTEM ANYMORE THROUGH EXECUTING MAKE OR
-            TAKE ORDERS, OR THROUGH THE INVEST FUNCTIONS.
-          </p>
-        </Fragment>
-      ) : (
-        <Fragment>
-          <p>
-            <b>Melon</b>, [méllō], μέλλω; Greek for <b>"destined to be"</b>:
-          </p>
-          <p>
-            Blockchain software that seeks to enable participants to set up,
-            manage and invest in technology regulated digital investment funds.
-          </p>
-          <p>
-            Set up your own technology regulated fund and discover the future of
-            asset management.
-          </p>
-        </Fragment>
-      )}
-      <Button onClick={onStart}>{linkCaption}</Button>
+
+      <div className="get-started__welcome">
+        <h1 className="get-started__title">
+          Welcome to <span className="get-started__name">Melon</span>
+        </h1>
+      </div>
+      <div className="get-started__description">
+        {networkId === '1' ? (
+          <Fragment>
+            <p>
+              <b>You are accessing Melon on the Ethereum main network.</b>
+            </p>
+            <p>
+              AT THE END OF THE TESTING PERIOD OR AT ANY ARBITRARY POINT IN TIME
+              AS DEFINED BY MELONPORT AG, THE SMART CONTRACT SYSTEM WILL BE
+              CLOSED DOWN. THIS IS EXPECTED TO HAPPEN IN A MATTER OF DAYS OR
+              WEEKS FROM DEPLOYMENT DATE. AT THIS POINT, THE USERS WON’T BE ABLE
+              TO INTERACT WITH THE THE SMART CONTRACT SYSTEM ANYMORE THROUGH
+              EXECUTING MAKE OR TAKE ORDERS, OR THROUGH THE INVEST FUNCTIONS.
+            </p>
+          </Fragment>
+        ) : (
+          <Fragment>
+            <p>
+              <b>Melon</b>, [méllō], μέλλω; Greek for <b>"destined to be"</b>:
+            </p>
+            <p>
+              Blockchain software that seeks to enable participants to set up,
+              manage and invest in technology regulated digital investment
+              funds.
+            </p>
+            <p>
+              Set up your own technology regulated fund and discover the future
+              of asset management.
+            </p>
+          </Fragment>
+        )}
+        <Button onClick={onStart}>{linkCaption}</Button>
+      </div>
     </div>
   );
 };
