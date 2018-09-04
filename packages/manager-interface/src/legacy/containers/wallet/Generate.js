ad6e9aa9948f6e624865a1f03655a1e2c122dbb6
Matthias Gemperli <hello@matthiasgemperli.ch>
2018-09-04 11:53:11 +0200

    Add restore wallet

--- packages/manager-interface/src/legacy/containers/wallet/Generate.js
+++ packages/manager-interface/src/legacy/containers/wallet/Generate.js
@@ -1,13 +1,40 @@
 import { connect } from 'react-redux';
 import { actions as routeActions } from '../../actions/routes';
-import Generate from '@melonproject/manager-components/components/GenerateWallet';
+import Generate from '@melonproject/manager-components/components/GenerateWallet/container';
+import { actions as walletAction } from '../../actions/wallet';
+import { importWalletFromMnemonic } from '@melonproject/melon.js';
 
 const mapStateToProps = state => ({
-  mnemonic: state.wallet.mnemonic,
+  generatedMnemonic: state.wallet.mnemonic,
+  restoreWallet: {
+    initialValues: {
+      mnemonic: '',
+    },
+  },
+  passwordForm: {
+    initialValues: {
+      password: '',
+    },
+  },
+  error: state.wallet.reason,
 });
 
+const getWallet = mnemonic => importWalletFromMnemonic(mnemonic);
+
 const mapDispatchToProps = dispatch => ({
-  restore: () => dispatch(routeActions.walletRestore({ onboarding: true })),
+  onSubmitMnemonic: async values => {
+    try {
+      importWalletFromMnemonic(values.mnemonic);
+      return dispatch(walletAction.restoreFromMnemonic(values.mnemonic));
+    } catch (err) {
+      return dispatch(
+        walletAction.restoreFromMnemonicFailed('Invalid BIP39 mnemonic'),
+      );
+    }
+  },
+  onSubmitPassword: values => {
+    dispatch(walletAction.passwordEntered(values.password));
+  },
 });
 
 export default connect(
--- packages/manager-interface/src/legacy/containers/wallet/Restore.js
+++ packages/manager-interface/src/legacy/containers/wallet/Restore.js
@@ -2,26 +2,33 @@ import { connect } from 'react-redux';
 import { importWalletFromMnemonic } from '@melonproject/melon.js';
 import { actions } from '../../actions/wallet';
 import RestoreWallet from '@melonproject/manager-components/components/RestoreWallet/container';
+import Generate from '@melonproject/manager-components/components/GenerateWallet/container';
 
 const mapStateToProps = state => ({
   initialValues: {
     mnemonic: '',
   },
   error: state.wallet.reason,
+  generate: false,
 });
 
 const mapDispatchToProps = dispatch => ({
-  onSubmit: values => {
+  onSubmitMnemonic: async values => {
     try {
       importWalletFromMnemonic(values.mnemonic);
-      dispatch(actions.restoreFromMnemonic(values.mnemonic));
+      return dispatch(actions.restoreFromMnemonic(values.mnemonic));
     } catch (err) {
-      dispatch(actions.restoreFromMnemonicFailed('Invalid BIP39 mnemonic'));
+      return dispatch(
+        actions.restoreFromMnemonicFailed('Invalid BIP39 mnemonic'),
+      );
     }
+  },
+  onSubmitPassword: values => {
+    dispatch(actions.passwordEntered(values.password));
   },
 });
 
 export default connect(
   mapStateToProps,
   mapDispatchToProps,
-)(RestoreWallet);
+)(Generate);
