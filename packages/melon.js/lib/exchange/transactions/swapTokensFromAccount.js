d1c2c405ca8e82e24e600e9a2e1cac6e32132ffb
GauthamN <gauthamnekk@gmail.com>
2018-09-07 17:49:59 +0200

    Add swapTokensFromAccount function

---
+++ packages/melon.js/lib/exchange/transactions/swapTokensFromAccount.js
@@ -0,0 +1,48 @@
+// @flow
+import getKyberProxyContract from '../contracts/getKyberProxyContract';
+import findEventInLog from '../../utils/ethereum/findEventInLog';
+import sendTransaction from '../../utils/parity/sendTransaction';
+import approve from '../../assets/transactions/approve';
+import getAddress from '../../assets/utils/getAddress';
+import getConfig from '../../version/calls/getConfig';
+
+/**
+ * Cancel an order by `id`
+ */
+const swapTokensFromAccount  = async (environment,
+  {
+    srcTokenSymbol,
+    srcAmount,
+    destTokenSymbol,
+    minConversionRate
+  }) => {
+
+  const config = await getConfig(environment);
+  const srcToken = getAddress(config, srcTokenSymbol);
+  const destToken = getAddress(config, destTokenSymbol);
+  const kyberProxyContract = await getKyberProxyContract(environment);
+
+  await approve(environment, {
+    symbol: srcTokenSymbol,
+    spender: kyberProxyContract.address,
+    quantity: srcAmount,
+  });
+
+  const receipt = await sendTransaction(
+    kyberProxyContract,
+    'swapTokenToToken',
+    [srcToken, srcAmount, destToken, minConversionRate],
+    environment,
+    {},
+  );
+
+  const executedEvent = findEventInLog(
+    'ExecuteTrade',
+    receipt,
+    'Error during swap tokens execution',
+  );
+
+  return executedEvent.params.actualDestAmount.value;
+};
+
+export default swapTokensFromAccount;
