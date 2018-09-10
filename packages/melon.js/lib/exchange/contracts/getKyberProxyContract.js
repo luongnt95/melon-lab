2ce44a722fbaad1f1058a61368c46deaa61c7c82
GauthamN <gauthamnekk@gmail.com>
2018-09-10 10:44:53 +0200

    Add getConversionRate function

--- packages/melon.js/lib/exchange/contracts/getKyberProxyContract.js
+++ packages/melon.js/lib/exchange/contracts/getKyberProxyContract.js
@@ -11,7 +11,7 @@ const getKyberProxyContract = async (environment: Environment) => {
   const config = await getConfig(environment);
   return environment.api.newContract(
     KyberProxyAbi,
-    config.kyberNetworkProxy,
+    "0x7e6b8b9510D71BF8EF0f893902EbB9C865eEF4Df",
   );
 };
 
