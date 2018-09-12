6d373c874daaa8743d03f5347a642266656d31b4
Matthias Gemperli <hello@matthiasgemperli.ch>
2018-09-12 10:29:04 +0200

    Refactor code

--- packages/manager-interface/src/components/GenerateWallet/index.tsx
+++ packages/manager-interface/src/components/GenerateWallet/index.tsx
@@ -6,20 +6,17 @@ import { Query } from 'react-apollo';
 const generateWalletQuery = gql`
   query GenerateWalletQuery {
     mnemonic @client
-    canonicalPriceFeedAddress @client
   }
 `;
 
 const withGenerateWallet = BaseComponent => baseProps => (
   <Query query={generateWalletQuery} ssr={false}>
-    {props =>
-      console.log(props) || (
-        <BaseComponent
-          mnemonic={props.data && props.data.mnemonic}
-          loading={props.loading}
-        />
-      )
-    }
+    {props => (
+      <BaseComponent
+        mnemonic={props.data && props.data.mnemonic}
+        loading={props.loading}
+      />
+    )}
   </Query>
 );
 
